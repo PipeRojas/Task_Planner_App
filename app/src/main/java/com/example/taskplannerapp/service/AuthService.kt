8eedcdc905ee6e782af3b399f6c9b61264f3393f
PipeRojas <andresf_135@hotmail.com>
2022-07-05 20:11:04 -0500

    Solucionado problema de dependencias

--- app/src/main/java/com/example/taskplannerapp/service/AuthService.kt
+++ app/src/main/java/com/example/taskplannerapp/service/AuthService.kt
@@ -1,11 +1,12 @@
 package com.example.taskplannerapp.service
 
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
 
     @POST("auth")
-    suspend fun authenticate(credentials: AuthRequest): Response<AuthResponse>
+    suspend fun authenticate(@Body credentials: AuthRequest): Response<AuthResponse>
 
 }
